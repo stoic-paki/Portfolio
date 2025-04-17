d94453939cf6ed0fcb3b504c2322e5ceb0c0591a
stoic-paki <taha.s200526@gmail.com>
2025-04-17 11:06:32 +1000

    adding vite with conditional rendering based on url

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react(), tailwindcss()],
+    base: '/',
+  }
 
-  plugins: [
-    tailwindcss(), react()],
+  if (command !== 'serve') {
+    config.base = '/Portfolio/'
+  }
 
-  base:"/Portfolio" ,
-
+  return config
 })
+
